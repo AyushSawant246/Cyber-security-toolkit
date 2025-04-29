3cab367313217c19670e48787ac6027811fa23b9
Ayush Sawant <ayushsawant246@gmail.com>
2025-04-29 23:44:29 +0530

    Update(2) por_scanner.py
    implemented service detection on each ports and added dynamic port range input

--- network_tools/port_scanner.py
+++ network_tools/port_scanner.py
@@ -1,20 +1,25 @@
 import socket
+import argparse
 
-# Target (you can hardcode or use input())
-target = input("Enter target IP or domain: ")
+parser = argparse.ArgumentParser(description="Basic Port Scanner")
 
-# Common ports to scan
-ports = [21, 22, 23, 25, 53, 80, 110, 139, 143, 443, 445, 3389]
+parser.add_argument("-t", "--target", help="Target IP address", required=True)
+parser.add_argument("-p", "--ports", help="Port range (e.g. 20-80)", default="1-100")
+
+args = parser.parse_args()
+target = args.target
+start_port, end_port = map(int, args.ports.split('-'))
 
 print(f"\n[+] Scanning target: {target}\n")
 
-for port in ports:
+for port in range(start_port, end_port + 1):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(1)  # Timeout for slow ports
         result = sock.connect_ex((target, port))
         if result == 0:
             print(f"[OPEN] Port {port}")
+            print(f"[OPEN] Port {port} ({socket.getservbyport(port)})")
         sock.close()
     except KeyboardInterrupt:
         print("\nExiting...")
