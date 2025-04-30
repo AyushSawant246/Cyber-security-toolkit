cc5978dcc33e2369dcbfff459d8bf6a457ff4e49
Ayush Sawant <ayushsawant246@gmail.com>
2025-04-30 22:57:43 +0530

    Update (2) network_sniffer.py
    added filter like HTTP (port 80), HTTPS (port 443), or TCP/UDP.

--- Network_sniffer/network_sniffer.py
+++ Network_sniffer/network_sniffer.py
@@ -1,6 +1,8 @@
 from scapy.all import sniff, IP
 packet_limit = int(input("Enter number of packets to capture (default 10): ") or 10)
 packets = sniff(count=packet_limit)
+user_filter = input("Enter filter (e.g., 'tcp port 80') or press Enter for no filter: ")
+packets = sniff(count=packet_limit, filter=user_filter)
 # Callback function to process captured packets
 def packet_callback(packet):
     if IP in packet:
